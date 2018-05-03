e36e8ac5b85d1d72ee1bc250ba92924358d614bd
Martin Berangere <martin.berangere@live.fr>
2018-05-03 14:40:20 +0200

    partie 1 ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,15 @@ class UsersController < ApplicationController
   end
 
   def create
+  	puts 'infos recupere'
+  	puts params.inspect
+  	@user = User.create(params.permit(:name))
+  	puts "info générée"
+  	puts @user.inspect
+  	redirect_to user_path(@user.id)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
